cb1242ee38e0f30cc64cc25f049659157d90780d
traysimay <teh@raptor.pizza>
2020-01-27 22:55:09 -0700

    Updates

---
+++ precache-manifest.998efbf7e3a90b2513157b989421a301.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60182ffb9d79ea3ec395b4b5f5fadeef",
+    "url": "traysimay.github.io/ceres2/index.html"
+  },
+  {
+    "revision": "facdbe36350e57b24789",
+    "url": "traysimay.github.io/ceres2/static/css/main.0036da2d.chunk.css"
+  },
+  {
+    "revision": "f6d8c68d8de81abd5624",
+    "url": "traysimay.github.io/ceres2/static/js/2.0f42e092.chunk.js"
+  },
+  {
+    "revision": "612d44d18a1c2d5bec888c20f7624873",
+    "url": "traysimay.github.io/ceres2/static/js/2.0f42e092.chunk.js.LICENSE"
+  },
+  {
+    "revision": "facdbe36350e57b24789",
+    "url": "traysimay.github.io/ceres2/static/js/main.22ebf8f6.chunk.js"
+  },
+  {
+    "revision": "f46cdba509c8ccfb6286",
+    "url": "traysimay.github.io/ceres2/static/js/runtime-main.85ba9d93.js"
+  },
+  {
+    "revision": "918b7612af1843d14fb8a7308f81dcf1",
+    "url": "traysimay.github.io/ceres2/static/media/Ari.918b7612.ttf"
+  },
+  {
+    "revision": "11fb02bfa4d794f4322c2a5a4b9dda0d",
+    "url": "traysimay.github.io/ceres2/static/media/Euclid.11fb02bf.ttf"
+  }
+]);
